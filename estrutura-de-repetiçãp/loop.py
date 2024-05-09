43340e9f16844038269041b02148fac9adc3b122
gokugamer2231 <159553148+IsadoraMariaDaSilvaTrindade@users.noreply.github.com>
2024-05-09 10:03:18 -0300

    Vermelho 5
    09/05/2024

---
+++ estrutura-de-repetiçãp/loop.py
@@ -0,0 +1,5 @@
+for numero_coluna1 in range(2,5):
+    print("Tabuada do", numero_coluna1)
+for numero_coluna2 in range(11):
+    print(numero_coluna1,"x",numero_coluna2,"=2",numero_coluna1*numero_coluna2)
+    
